a79d7cfb8422d5b1eb9f017a7dba6334dab9a69c
sealfury <willspecker@gmail.com>
2021-09-22 17:00:37 +0200

    adds static / document properties to user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,10 +1,21 @@
-import mongoose from 'mongoose' 
+import mongoose from 'mongoose'
 
 // Describes required props to create new User
 interface UserAttrs {
   email: string
   password: string
 }
+
+// Describes props that a User model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc
+}
+
+// Describes props that a User document has
+interface UserDoc extends mongoose.Document {
+  email: string
+  password: string
+} 
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -16,12 +27,16 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 })
-
-const User = mongoose.model('User', userSchema)
-
 // replaces call to new User()
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs)
 }
 
-export { User, buildUser }
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
+
+const user = User.build({
+  email: 'test@test.com',
+  password: 'password'
+})
+
+export { User }
