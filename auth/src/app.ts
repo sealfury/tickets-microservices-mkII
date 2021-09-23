d8ca8fecaaaaa4862866097bf0290cb4ff6fa052
sealfury <willspecker@gmail.com>
2021-09-23 17:31:16 +0200

    allows insecure cookie session use in test env writes a few more signup tests

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -16,7 +16,7 @@ app.use(json())
 app.use(
   cookieSession({
     signed: false, // will encrypt w/ JWT instead
-    secure: true,
+    secure: process.env.NODE_ENV !== 'test',
   })
 )
 
--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -46,3 +46,33 @@ it('should return a 400 response with missing email & password', async () => {
     })
     .expect(400)
 })
+
+it('should disallow duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'mail@mail.com',
+      password: 'password',
+    })
+    .expect(201)
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'mail@mail.com',
+      password: 'password',
+    })
+    .expect(400)
+})
+
+it('should set a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'mail@mail.com',
+      password: 'password',
+    })
+    .expect(201)
+
+  expect(response.get('Set-Cookie')).toBeDefined()
+})
