bcbdf7f5b905a88c7da61b77bf876c2c591cac30
sealfury <willspecker@gmail.com>
2021-09-23 18:15:09 +0200

    creates global auth cookie test function tests some current user requests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,19 +1,25 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
+import request from 'supertest'
 import { app } from '../app'
+
+// Alert TS to global signin property
+declare global {
+  var getAuthCookie: () => Promise<string[]>
+}
 
 let mongo: any
 // mock mongo db
 beforeAll(async () => {
   process.env.JWT_KEY = 'test-key'
-  /* 
-  * This was the previous syntax:
-  *  mongo = new MongoMemoryServer()
-  *  const mongoUri = await mongo.getUri()
-  * MongoMemoryServer v7.0.0 requires changes made below
-  * if changes cause any issues in future downgrade MMS to v6.9.6
-  * and replace with previous syntax
-  */
+  /*
+   * This was the previous syntax:
+   *   mongo = new MongoMemoryServer()
+   *   const mongoUri = await mongo.getUri()
+   * MongoMemoryServer v7.0.0 requires changes made below
+   * if changes cause any issues in future, downgrade MMS to v6.9.6
+   * and replace with previous syntax
+   */
   mongo = await MongoMemoryServer.create()
   const mongoUri = mongo.getUri()
 
@@ -34,3 +40,21 @@ afterAll(async () => {
   await mongo.stop()
   await mongoose.connection.close()
 })
+
+// Global auth helper function
+global.getAuthCookie = async () => {
+  const email = 'mail@mail.com'
+  const password = 'password'
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201)
+
+  const cookie = response.get('Set-Cookie')
+
+  return cookie
+}
