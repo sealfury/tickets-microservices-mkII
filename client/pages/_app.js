addabae491eef9a9fe35f089f0bdc96afc3fa560
sealfury <willspecker@gmail.com>
2021-09-24 09:40:31 +0200

    constructs sign up component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
-export default ({ Component, pageProps }) => {
+const _App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
+
+export default _App
