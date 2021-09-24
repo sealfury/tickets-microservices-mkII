fefa1221848148f65eb961b28aef9acaad27a410
sealfury <willspecker@gmail.com>
2021-09-24 14:25:10 +0200

    updates imports for elements now coming from common module

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,13 +2,12 @@ import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
 import cookieSession from 'cookie-session'
+import { errorHandler, NotFoundError } from '@sealtix/common'
 
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
-import { errorHandler } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 app.set('trust proxy', true) // trust ingress-nginx
@@ -25,9 +24,9 @@ app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
 
-// app.all('*', async (req, res) => {
-//   throw new NotFoundError()
-// })
+app.all('*', async (req, res) => {
+  throw new NotFoundError()
+})
 
 app.use(errorHandler)
 
