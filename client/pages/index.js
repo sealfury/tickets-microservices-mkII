3446b62d02150e4ccff6e37229e0eec6aca53c0d
sealfury <willspecker@gmail.com>
2021-09-24 11:07:37 +0200

    specifies host for conditional rendering of props

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,31 @@
-const LandingPage = () => {
+import axios from 'axios'
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser)
   return <h1>Landing Page</h1>
+}
+
+LandingPage.getInitialProps = async ({ req }) => {
+  const INGRESS_SRV_CLUSTER_URL =
+    'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
+
+  if (typeof window === 'undefined') {
+    // on the server - requests to ingress-nginx namespace
+    const { data } = await axios.get(
+      `${INGRESS_SRV_CLUSTER_URL}/api/users/currentuser`,
+      {
+        headers: req.headers,
+      }
+    )
+
+    return data
+  } else {
+    // in the browser - requests made w/ baseURL of ''
+    const { data } = await axios.get('/api/users/currentuser')
+
+    return data
+  }
+  return {}
 }
 
 export default LandingPage
