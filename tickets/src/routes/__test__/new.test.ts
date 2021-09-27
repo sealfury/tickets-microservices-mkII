4b244ad6bf53fd1b65deb29cdb2ffcb89b48c704
sealfury <willspecker@gmail.com>
2021-09-27 13:04:43 +0200

    adds validations for title and price

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -20,8 +20,53 @@ it('should return a status other than 401 if the user is signed in', async () =>
   expect(response.status).not.toEqual(401)
 })
 
-it('should return an error if an invalid title is provided', async () => {})
+it('should return an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.getAuthCookie())
+    .send({
+      title: '',
+      price: 20,
+    })
+    .expect(400)
 
-it('should return an error if an invalid price is provided', async () => {})
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.getAuthCookie())
+    .send({
+      price: 20,
+    })
+    .expect(400)
+})
 
-it('should create a ticket with provided with valid inputs', async () => {})
+it('should return an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.getAuthCookie())
+    .send({
+      title: 'testTicket',
+      price: -20,
+    })
+    .expect(400)
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.getAuthCookie())
+    .send({
+      title: 'testTicket',
+    })
+    .expect(400)
+})
+
+it('should create a ticket with provided with valid inputs', async () => {
+  // add a check to make sure a ticket was saved
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.getAuthCookie())
+    .send({
+      title: 'testTicket',
+      price: 20,
+    })
+    .expect(201)
+})
