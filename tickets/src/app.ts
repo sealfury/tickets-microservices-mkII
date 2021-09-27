72007b0a667becf730e3c05e92c15ce48617b79c
sealfury <willspecker@gmail.com>
2021-09-27 14:11:43 +0200

    implements get all tickets (index) route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import { errorHandler, NotFoundError, currentUser } from '@sealtix/common'
 
 import { createTicketRouter } from './routes/new'
 import { showTicketRouter } from './routes/show'
+import { indexTicketRouter } from './routes/index'
 
 const app = express()
 app.set('trust proxy', true) // trust ingress-nginx
@@ -20,6 +21,7 @@ app.use(currentUser)
 
 app.use(createTicketRouter)
 app.use(showTicketRouter)
+app.use(indexTicketRouter)
 
 app.all('*', async (req, res) => {
   throw new NotFoundError()
