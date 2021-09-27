8bd6cd5feb4b136cccbaaba9114f1831dd83e7ec
sealfury <willspecker@gmail.com>
2021-09-27 14:03:40 +0200

    completes show route implementation/test adds helper test function for mock ticket id updates common module (logs unexpected errors)

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError, currentUser } from '@sealtix/common'
 
 import { createTicketRouter } from './routes/new'
+import { showTicketRouter } from './routes/show'
 
 const app = express()
 app.set('trust proxy', true) // trust ingress-nginx
@@ -18,6 +19,7 @@ app.use(
 app.use(currentUser)
 
 app.use(createTicketRouter)
+app.use(showTicketRouter)
 
 app.all('*', async (req, res) => {
   throw new NotFoundError()
