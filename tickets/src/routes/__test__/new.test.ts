803604a32ebffcec602b5db83fad7a14e47b7dba
sealfury <willspecker@gmail.com>
2021-09-27 13:24:09 +0200

    finishes create ticket request handler

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import { app } from '../../app'
+import { Ticket } from '../../models/ticket'
 
 it('should have a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({})
@@ -59,14 +60,23 @@ it('should return an error if an invalid price is provided', async () => {
 })
 
 it('should create a ticket with provided with valid inputs', async () => {
-  // add a check to make sure a ticket was saved
+  // a check to make sure a ticket was saved
+  let tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(0)
+
+  const title = 'testTicket'
 
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.getAuthCookie())
     .send({
-      title: 'testTicket',
+      title,
       price: 20,
     })
     .expect(201)
+
+  tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(1)
+  expect(tickets[0].title).toEqual(title)
+  expect(tickets[0].price).toEqual(20)
 })
