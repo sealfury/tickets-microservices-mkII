16cec9672b1ff33e6a0173addee95d8e8526ca6e
sealfury <willspecker@gmail.com>
2021-09-28 12:14:20 +0200

    extracts nats env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,12 +12,22 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined!')
   }
+  // Type checks for NATS env variables
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined!')
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined!')
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined!')
+  }
 
   try {
     await natsWrapper.connect(
-      'ticketing',
-      'randomString',
-      'http://nats-srv:4222'
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
     )
     // Graceful shutdown
     natsWrapper.client.on('close', () => {
