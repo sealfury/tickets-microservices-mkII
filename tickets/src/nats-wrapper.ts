f355d49a509faacc995034449bf2622d46576a64
sealfury <willspecker@gmail.com>
2021-09-28 10:39:41 +0200

    implements access to nats client via singleton

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -3,16 +3,25 @@ import nats, { Stan } from 'node-nats-streaming'
 class NatsWrapper {
   private _client?: Stan
 
+  // prevent access to client until connect is run
+  get client() {
+    if (!this._client) {
+      throw new Error('Cannot access NATS client before connecting!')
+    }
+
+    return this._client
+  }
+
   connect(clusterId: string, clientId: string, url: string) {
     this._client = nats.connect(clusterId, clientId, { url })
 
     // adjust nats connect function to make it async (like mongoose)
     return new Promise<void>((resolve, reject) => {
-      this._client!.on('connect', () => {
+      this.client.on('connect', () => {
         console.log('Connected to NATS')
         resolve()
       })
-      this._client!.on('error', err => {
+      this.client.on('error', err => {
         reject(err)
       })
     })
