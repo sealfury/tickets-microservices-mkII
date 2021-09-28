81481726158d673b71ce00fb0e69d33cbf260734
sealfury <willspecker@gmail.com>
2021-09-28 09:43:20 +0200

    creates publisher for ticket creation events

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator'
 import { requireAuth, validateRequest } from '@sealtix/common'
 
 import { Ticket } from '../models/ticket'
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher'
 
 const router = express.Router()
 
@@ -25,6 +26,13 @@ router.post(
       userId: req.currentUser!.id,
     })
     await ticket.save()
+    
+    new TicketCreatedPublisher(client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId
+    })
 
     res.status(201).send(ticket)
   }
