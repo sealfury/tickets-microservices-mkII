a91ecb21c880a5b37112158d339032c1ef03d758
sealfury <willspecker@gmail.com>
2021-09-28 16:10:53 +0200

    implements essential create functionality to new route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,18 +1,29 @@
 import mongoose from 'mongoose'
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
-import { requireAuth, validateRequest } from '@sealtix/common'
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@sealtix/common'
+
+import { Ticket } from '../models/ticket'
+import { Order } from '../models/order'
 
 const router = express.Router()
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60
 
 router.post(
   '/api/orders',
   requireAuth,
   [
     /*
-    * OBS! If Tickets DB changes or if Orders Service subscribes
-    * to other non-mongo DBs remove or adjust custom check
-    */
+     * OBS! If Tickets DB changes or if Orders Service subscribes
+     * to other non-mongo DBs, remove or adjust custom check
+     */
     body('ticketId')
       .not()
       .isEmpty()
@@ -21,7 +32,36 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({})
+    const { ticketId } = req.body
+
+    // Find ticket the user is trying to order in the DB
+    const ticket = await Ticket.findById(ticketId)
+    if (!ticket) {
+      throw new NotFoundError()
+    }
+
+    // Ensure that ticket isn't reserved
+    const isReserved = await ticket.isReserved()
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved!')
+    }
+
+    // Calculate an expiration date for the order
+    const expiration = new Date()
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
+
+    // Build the order and save it to DB
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    })
+    await order.save()
+
+    // TODO: Publish an order created event
+
+    res.status(201).send(order)
   }
 )
 
