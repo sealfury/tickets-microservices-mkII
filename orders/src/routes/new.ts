f671896a354367e6a1c132ff33cdb07b990d51a5
sealfury <willspecker@gmail.com>
2021-09-28 13:55:35 +0200

    sets up order model & adds validation checks to create route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,28 @@
+import mongoose from 'mongoose'
 import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
+import { requireAuth, validateRequest } from '@sealtix/common'
 
 const router = express.Router()
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({})
-})
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    /*
+    * OBS! If Tickets DB changes or if Orders Service subscribes
+    * to other non-mongo DBs remove or adjust custom check
+    */
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided!'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({})
+  }
+)
 
 export { router as createOrderRouter }
