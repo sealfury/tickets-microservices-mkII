f960fba8b65b62affd9696e5ca06bf43f7b5c4fe
sealfury <willspecker@gmail.com>
2021-09-29 11:17:32 +0200

    implements show route handler

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,38 @@
 import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
+import mongoose from 'mongoose'
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@sealtix/common'
+
+import { Order } from '../models/order'
 
 const router = express.Router()
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({})
-})
+router.get(
+  '/api/orders/:orderId',
+  requireAuth,
+  [
+    /*
+     * OBS! If Orders DB changes or if concurrent services subscribe
+     * to other non-mongo DBs, remove or adjust custom check
+     */
+    body('orderId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('OrderId must be provided!'),
+  ],
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket')
+
+    if (!order) {
+      throw new NotFoundError()
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError()
+    }
+
+    res.send(order)
+  }
+)
 
 export { router as showOrderRouter }
