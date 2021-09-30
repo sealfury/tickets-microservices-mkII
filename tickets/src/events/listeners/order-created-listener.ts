468f7d47bb48f54b20e8a945f784fc8263dc5115
sealfury <willspecker@gmail.com>
2021-09-30 14:18:01 +0200

    scaffolds order created listener & organizes exports

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,13 @@
+import { Message } from 'node-nats-streaming'
+import { Listener, OrderCreatedEvent, Subjects } from "@sealtix/common";
+
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated
+  queueGroupName = queueGroupName
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    
+  }
+}
