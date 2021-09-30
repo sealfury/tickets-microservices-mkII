826a735afc999fd8c72cf0d18514f00f6159ee4e
sealfury <willspecker@gmail.com>
2021-09-30 10:43:42 +0200

    updates ticket event defs w/ version attr

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -31,6 +31,7 @@ router.post(
     // Emit created event
     new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
+      version: ticket.version,
       title: ticket.title,
       price: ticket.price,
       userId: ticket.userId,
