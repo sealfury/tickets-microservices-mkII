084162402dcea3ce79b42e8a1e457723e0fd6582
sealfury <willspecker@gmail.com>
2021-09-30 19:26:43 +0200

    implements delay functionality in expiration queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -8,9 +8,18 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    await expirationQueue.add({
-      orderId: data.id
-    })
+    // (time of expiry in ms) - (current time in ms)
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime()
+    console.log(`Waiting ${delay} ms to process the time`)
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    )
 
     msg.ack()
   }
