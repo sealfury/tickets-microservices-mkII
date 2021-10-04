02de94dbcf2b5d3456cc15a0b57f19beb2bad7da
sealfury <willspecker@gmail.com>
2021-10-04 13:12:27 +0200

    mocks stripe client and writes initial test for payment route

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,9 @@ import mongoose from 'mongoose'
 import { app } from '../../app'
 import { Order } from '../../models/order'
 import { OrderStatus } from '@sealtix/common'
+import { stripe } from '../../stripe'
+
+jest.mock('../../stripe')
 
 const randomId = new mongoose.Types.ObjectId().toHexString()
 
@@ -38,7 +41,7 @@ it('should return a 401 when a user attempts to purchase an order that does not 
 })
 
 it('should return a 400 when user attempts to purchase a cancelled order', async () => {
-  const userId = randomId 
+  const userId = randomId
   const order = Order.build({
     id: randomId,
     userId: randomId,
@@ -53,7 +56,34 @@ it('should return a 400 when user attempts to purchase a cancelled order', async
     .set('Cookie', global.getAuthCookie(userId))
     .send({
       orderId: order.id,
-      token: 'tempToken'
+      token: 'tempToken',
     })
     .expect(400)
+})
+
+it('should return a 204 with valid inputs to charge event', async () => {
+  const userId = randomId
+  const order = Order.build({
+    id: randomId,
+    userId: randomId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  })
+  await order.save()
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.getAuthCookie(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
+    .expect(201)
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0]
+
+  expect(chargeOptions.source).toEqual('tok_visa')
+  expect(chargeOptions.amount).toEqual(20 * 100)
+  expect(chargeOptions.currency).toEqual('usd')
 })
