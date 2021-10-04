b8871a0daeffded09f214bda80854f9db322bc0d
sealfury <willspecker@gmail.com>
2021-10-04 19:28:08 +0200

    implements expiration timer in order show

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,34 @@
-const OrderShow = () => {
-  return <div>OrderShow</div>
+import { useEffect, useState } from 'react'
+
+const OrderShow = ({ order }) => {
+  const [timeRemaining, setTimeRemaining] = useState(0)
+
+  useEffect(() => {
+    const findTimeRemaining = () => {
+      const msRemaining = new Date(order.expiresAt) - new Date()
+      setTimeRemaining(Math.round(msRemaining / 1000))
+    }
+
+    findTimeRemaining()
+    const timerId = setInterval(findTimeRemaining, 1000)
+
+    return () => {
+      clearInterval(timerId)
+    }
+  }, [])
+
+  if (timeRemaining < 0) {
+    return <div>Order Expired!</div>
+  }
+
+  return <div>Time to pay: {timeRemaining} seconds</div>
+}
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query
+  const { data } = await client.get(`/api/orders/${orderId}`)
+
+  return { order: data }
 }
 
 export default OrderShow
