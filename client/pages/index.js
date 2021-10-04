fd6bda96850edff41a92fabc2756f8a0bfdd67d2
sealfury <willspecker@gmail.com>
2021-10-04 16:04:31 +0200

    extracts currentUser data & buildClient functionality to page props to avoid repeated calls to both

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,3 @@
-import buildClient from '../api/build-client'
-
 const LandingPage = ({ currentUser }) => {
   return (
     <h1>
@@ -10,11 +8,8 @@ const LandingPage = ({ currentUser }) => {
   )
 }
 
-LandingPage.getInitialProps = async context => {
-  const client = buildClient(context)
-  const { data } = await client.get('/api/users/currentuser')
-
-  return data
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {}
 }
 
 export default LandingPage
