5d83f7516143fef2aea672b8fed34c97c7c8ba5e
sealfury <willspecker@gmail.com>
2021-10-04 20:16:46 +0200

    implements essential payment functionality

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,18 @@
 import { useEffect, useState } from 'react'
 import StripeCheckout from 'react-stripe-checkout'
 
+import useRequest from '../../hooks/use-request'
+
 const OrderShow = ({ order, currentUser }) => {
   const [timeRemaining, setTimeRemaining] = useState(0)
+  const { makeRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: payment => console.log(payment),
+  })
 
   useEffect(() => {
     const findTimeRemaining = () => {
@@ -26,11 +36,12 @@ const OrderShow = ({ order, currentUser }) => {
     <div>
       Time to pay: {timeRemaining} seconds
       <StripeCheckout
-        token={token => console.log(token)}
+        token={({ id }) => makeRequest({ token: id })}
         stripeKey={process.env.STRIPE_PUBLIC_KEY}
         amount={order.ticket.price * 100}
         email={currentUser.email}
       />
+      {errors}
     </div>
   )
 }
