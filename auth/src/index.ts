fb75a5dc91caf26cd167488cf760ed294ea8c7ee
sealfury <willspecker@gmail.com>
2021-10-05 11:08:34 +0200

    adds startup msg

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import { app } from './app'
 
 const start = async () => {
+  console.log('Starting...')
   // Type check for JWT_KEY in all routes
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined!')
