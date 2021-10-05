83a9580a98985df9bda7cacb1c408291aba20b00
sealfury <willspecker@gmail.com>
2021-10-05 15:18:33 +0200

    updates base url

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,14 +1,13 @@
 import axios from 'axios'
 
 const buildClient = ({ req }) => {
-  const INGRESS_SRV_CLUSTER_URL =
-    'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
+  const BASE_URL = 'http://www.sealtix.xyz/'
 
   if (typeof window === 'undefined') {
     // on the server - requests to ingress-nginx namespace
     // configure axios with service URL and headers
     return axios.create({
-      baseURL: INGRESS_SRV_CLUSTER_URL,
+      baseURL: BASE_URL,
       headers: req.headers,
     })
   } else {
