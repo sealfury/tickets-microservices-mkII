3c44163b683825c86148cd715831537b07630ddc
sealfury <willspecker@gmail.com>
2021-10-05 14:02:21 +0200

    streamlines order cancelled listener tests

--- tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -33,38 +33,13 @@ const setup = async () => {
   return { listener, orderId, ticket, data, msg }
 }
 
-it('should update ticket being cancelled', async () => {
-  const { listener, orderId, ticket, data, msg } = await setup()
+it('should update the ticket, publish an event, and ack the message', async () => {
+  const { msg, data, ticket, orderId, listener } = await setup()
 
   await listener.onMessage(data, msg)
 
   const updatedTicket = await Ticket.findById(ticket.id)
   expect(updatedTicket!.orderId).not.toBeDefined()
-})
-
-it('should publish a ticket updated event when order is cancelled', async () => {
-  const { listener, orderId, ticket, data, msg } = await setup()
-
-  await listener.onMessage(data, msg)
-
-  expect(natsWrapper.client.publish).toHaveBeenCalled()
-
-  const publishedTicketData = JSON.parse(
-    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
-  )
-
-  expect(data.id).toEqual(publishedTicketData.orderId)
-
-  const publishedEvent = (natsWrapper.client.publish as jest.Mock).mock
-    .calls[0][0]
-
-  expect(publishedEvent).toEqual('ticket:updated')
-})
-
-it('should ack the message upon successful cancellation', async () => {
-  const { listener, data, msg } = await setup()
-
-  await listener.onMessage(data, msg)
-
   expect(msg.ack).toHaveBeenCalled()
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
 })
